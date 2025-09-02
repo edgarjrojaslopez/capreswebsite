8ae03f19786a993ed35eae0786d341be7f2e86ca
Edgar Jose Rojas Lopez <erojas@capres.com.ve>
2025-09-01 21:16:12 -0400

    fix: adding message to 'Tipos de Préstamo' and 'Tipo de Retiro'

--- components/DashboardContent.jsx
+++ components/DashboardContent.jsx
@@ -973,16 +973,23 @@ function LoanTypeModal({ onClose, onSelectType }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-3xl shadow-2xl max-w-2xl w-full p-7">
-        <div className="flex justify-between items-center mb-6">
+        <div className="flex justify-between items-center mb-4">
           <h3 className="text-2xl font-bold text-gray-800">
             💰 Tipos de Préstamo
           </h3>
+
           <button
             onClick={onClose}
             className="text-gray-500 hover:text-gray-700 text-2xl"
           >
             ×
           </button>
+        </div>
+        <div className="mb-6 text-gray-600">
+          <p>
+            Por favor verifique su cuenta de nómina en el lapso de 24 a 48
+            horas, una vez que haya hecho la solicitud.{' '}
+          </p>
         </div>
         <div className="grid gap-4">
           {loanTypes.map((type) => (
@@ -1110,6 +1117,12 @@ function RetiroTypeModal({
           >
             ×
           </button>
+        </div>
+        <div className="mb-6 text-gray-600">
+          <p>
+            Por favor verifique su cuenta de nómina en el lapso de 24 a 48
+            horas, una vez que haya hecho la solicitud.{' '}
+          </p>
         </div>
         <div className="grid gap-4">
           {tipos.map((tipo) => (
